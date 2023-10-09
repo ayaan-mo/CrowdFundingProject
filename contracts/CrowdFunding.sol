2eadafa05dde7a9ac3380bbc297f5127b6752c42
Ayaan <ayaan06@my.yorku.ca>
2023-10-09 11:34:32 -0400

    function 1 added

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -7,17 +7,49 @@ contract CrowdFunding {
         string title;
         string description;
         uint256 target;
-        uint256 deadlin;
+        uint256 deadline;
         uint256 amountCollected;
         string image;
         address[] donators;
         uint256[] donations;
-        
+
     }
 
     mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, 
+    uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadlin < block.timestamp, "The deadline should be a date in the future");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+
+    }
+
+    function donateToCampaign(){
+
+    }
+
+    function getDonators(){
+        
+    }
+
+    function getCampaigns(){
+        
+    }
 
 
 }
